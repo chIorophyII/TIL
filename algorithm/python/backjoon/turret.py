498dc86810895de07d0930106c0cdc15f151a216
chIorophyII <dab2in79@gmail.com>
2022-04-30 23:03:27 +0900

    turret python

--- algorithm/python/backjoon/turret.py
+++ algorithm/python/backjoon/turret.py
@@ -1,3 +1,16 @@
+import math
+
 num = int(input())
 
-print(num)
+for i in range(num):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split(" "))
+    length = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+    if (length == 0) and (r1 == r2):
+        print(-1)
+    elif length > (r1 + r2) or length < abs(r2 - r1):
+        print(0)
+    elif length == (r1 + r2) or length == abs(r2 - r1):
+        print(1)
+    else:
+        print(2)
